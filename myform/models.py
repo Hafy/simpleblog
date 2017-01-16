b83de9093608956d0279ef1ea19d031cfa827d44
phf <18734131050@163.com>
2017-01-16 16:15:13 +0800

    maybe the last commit

--- myform/models.py
+++ myform/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 # Create your models here.
 class Theme(models.Model):
-	theme_name=models.CharField(max_length=30)
+	theme_name=models.CharField(max_length=30,default='null')
 	class Meta:
 		db_table="theme"
 	def __str__(self):
 		return self.theme_name
 class Article(models.Model):
-	themes=models.ManyToManyField(Theme)
+	themes=models.ManyToManyField(Theme,blank=True,null=True)
 	pub_date=models.DateField(auto_now=True)
 	headline=models.CharField(max_length=100)
 	content=models.TextField()
